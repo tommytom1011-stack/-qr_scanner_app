24f9537cbe40b98042df9d562e2c54596af16af9
tommytom1011-stack <tommytom1011@gmail.com>
2026-03-03 12:28:49 +1100

    Update useScanner.js

--- QR Code COder/src/hooks/useScanner.js
+++ QR Code COder/src/hooks/useScanner.js
@@ -1,25 +1,24 @@
-
 import { useEffect, useState, useRef } from 'react';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
 
-export function useScanner(batch=false){
+export function useScanner(batch = false) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const scanningRef = useRef(false);
 
-  useEffect(()=>{
-    (async ()=>{
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
-  },[]);
+  }, []);
 
   const onScanned = (handler) => async ({ type, data }) => {
     if (scanningRef.current) return;
     scanningRef.current = true;
     setScanned(true);
-    await handler({type, data});
-    if (batch){
+    await handler({ type, data });
+    if (batch) {
       setScanned(false);
       scanningRef.current = false;
     }
